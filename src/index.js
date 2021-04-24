9613f6141ca54ded02b18910cc946d762305fb5a
MhmadTomy <tomymaulidu@gmail.com>
2021-04-25 00:05:07 +0700

    1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./style.css";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <Main/>,
+  document.getElementById("root")
+);
