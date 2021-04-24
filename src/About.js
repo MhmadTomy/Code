07d5d665e744b9096e3250262050918298be4dd9
MhmadTomy <32662458+MhmadTomy@users.noreply.github.com>
2021-04-25 00:12:11 +0700

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About Me</h2>
+        <p>Muhammad Tomy Nur Maulidy</p>
+        <p>083169538117</p>
+        <p>tomymaulidu@gmail.com</p>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+
+
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1 className="title">SOAL TURNKEY</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+          </div>
+        </div>
+        </HashRouter>
+    );
+  }
+}
+
+export default Main;
